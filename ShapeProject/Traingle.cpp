40ac6698323cbd7704261957f2a38f70eb73343c
Singh1289 <singh.1289abhi@gmail.com>
2024-07-10 18:55:52 +0530

    cpp cheats for everyone

---
+++ ShapeProject/Traingle.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+#include<string>
+#include"Triangle.h"
+using namespace std;
+
+Triangle::Triangle()
+{
+    base=0;
+    height=0;
+}
+
+Triangle::Triangle(char* nm,int b, int h):Shape(nm)
+{
+   base=b;
+   height=h;
+}
+Triangle::~Triangle()
+{
+    cout<<"\n Triangle Destruction called.";
+}
+
+float Triangle::area()
+{
+    return base*height;
+}
